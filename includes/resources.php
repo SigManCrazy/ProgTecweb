bccfb81b385d94b4752f8193ac8bc03e8030a702
Rollulp <rolando.sartorello@gmail.com>
2019-10-11 20:48:04 +0200

    aggiunti file default e schema cartelle

---
+++ includes/resources.php
@@ -0,0 +1,3 @@
+// qui si mettono tutti gli include, così richiamando questo file si caricano tutti
+require_once 'config.php'
+require_once 'variables.php'
---
+++ includes/variables.php
@@ -0,0 +1,8 @@
+// variabili di ambiente necessarie ovunque
+
+/* esempio :
+if(isset($_GET['id']))
+	$id = (int)$_GET['id0'];
+else
+	$id = 0;
+*/
