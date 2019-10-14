04a9e3c0c9b0d93d79fbefe6396220e05d8e9da1
Rollulp <rolando.sartorello@gmail.com>
2019-10-14 09:35:51 +0200

    change index name

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ define('SITE_ROOT', getcwd());
 include SITE_ROOT."/includes/resources.php";
 
 echo render("template.xhtml", [
-    "nome" => "AIFUIAWHIUFWHAUIOFD",
+    "nome" => "Pierino va a squola",
     "titolo" => "p. prova"
     ]);
 
