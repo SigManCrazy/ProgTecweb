3af6aa8e4906e117d8f717bc600e8c69d740fa0b
Rollulp <rolando.sartorello@gmail.com>
2019-12-10 18:52:54 +0100

    fix id=null

--- includes/pages/grappa.php
+++ includes/pages/grappa.php
@@ -5,15 +5,15 @@ include SITE_ROOT."/includes/resources.php";
 
 $db = new Database;
 $result = null;
-if ( isset($_GET["q"]) ){
-	$result = $db("select name, img_path, product.desc from product where id like ?", [ "%".$_GET["q"]."%" ]);
-	$res = $result->fetch();
-	$indexPage = build_default_page("pages/grappa" , [
-		"name"=> $res[0],
-		"desc"=> $res[2],
-		"src" => $res[1]
-]);}
-
+if ( ! isset($_GET["q"]) )
+	$_GET["q"] = 1;
+$result = $db("select name, img_path, product.desc from product where id like ?", [ "%".$_GET["q"]."%" ]);
+$res = $result->fetch();
+$indexPage = build_default_page("pages/grappa" , [
+	"name"=> $res[0],
+	"desc"=> $res[2],
+	"src" => $res[1]
+]);
 
 
 echo $indexPage->render();
