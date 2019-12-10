97bac2a41a38a1df0cbe8e02372ed89812c19fc5
Rollulp <rolando.sartorello@gmail.com>
2019-12-10 19:23:43 +0100

    aside linkato, aggiornata grappa.php-xhtml-css

--- includes/aside.php
+++ includes/aside.php
@@ -3,7 +3,7 @@
 function getAsideContentArray() {
 
 	$db = new Database();
-	$result = $db("select name, text, `when` from product join (select fk_product, text, `when` from comment) as p on product.id=p.fk_product order by p.`when` desc limit 6", []);
+	$result = $db("select product.ID, name, text, `when` from product join (select fk_product, text, `when` from comment) as p on product.id=p.fk_product order by p.`when` desc limit 6", []);
 	$c = [	$result->fetch(),
 			$result->fetch(),
 			$result->fetch(),
@@ -11,20 +11,25 @@ function getAsideContentArray() {
 			$result->fetch(),
 			$result->fetch(),
 			$result->fetch() ];
-	$i = 0;
     return [
-        "title1" => $c[$i][0],
-        "comment1" => substr($c[$i++][1], 0, 220),
-        "title2" => $c[$i][0],
-        "comment2" => substr($c[$i++][1], 0, 220),
-        "title3" => $c[$i][0],
-        "comment3" => substr($c[$i++][1], 0, 220),
-        "title4" => $c[$i][0],
-        "comment4" => substr($c[$i++][1], 0, 220),
-        "title5" => $c[$i][0],
-        "comment5" => substr($c[$i++][1], 0, 220),
-        "title6" => $c[$i][0],
-        "comment6" => substr($c[$i++][1], 0, 220),
+		"href1" => $c[0][0],
+        "title1" => $c[0][1],
+        "comment1" => substr($c[0][2], 0, 220),
+		"href2" => $c[1][0],
+        "title2" => $c[1][1],
+        "comment2" => substr($c[1][2], 0, 220),
+		"href3" => $c[2][0],
+        "title3" => $c[2][1],
+        "comment3" => substr($c[2][2], 0, 220),
+		"href4" => $c[3][0],
+        "title4" => $c[3][1],
+        "comment4" => substr($c[3][2], 0, 220),
+		"href5" => $c[4][0],
+        "title5" => $c[4][1],
+        "comment5" => substr($c[4][2], 0, 220),
+		"href6" => $c[5][0],
+        "title6" => $c[5][1],
+        "comment6" => substr($c[5][2], 0, 220),
     ];
 }
 
