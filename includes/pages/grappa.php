c9eb322bc6e611dcf43eeb38b8d39bb980492086
Rollulp <rolando.sartorello@gmail.com>
2019-12-11 18:25:26 +0100

    grappa detail modifiche massiccie

--- includes/pages/grappa.php
+++ includes/pages/grappa.php
@@ -7,16 +7,16 @@ $db = new Database;
 $result = null;
 if ( ! isset($_GET["id"]) )
 	$_GET["id"] = 1;
-$result = $db("select name, img_path, product.desc from product where id like ?", [ "%".$_GET["id"]."%" ]);
+$result = $db("SELECT name, img_path, product.desc FROM product WHERE id LIKE ?", [ "%".$_GET["id"]."%" ]);
 $res = $result->fetch();
 $page_num = 1;
 if ( isset($_GET["page"]) )
 	$page_num = $_GET["page"];
 if (!is_numeric($page_num)) // per injection check
 	$page_num = 1;
-$comments = $db("select p.text, username from account join " .
-	"(select text, fk_account, `when` from comment where fk_product=? order by `when` limit 5 offset ".$page_num.") as p on (account.ID=p.fk_account)" .
-	"order by p.`when`", [ $_GET["id"] ] );
+$comments = $db("SELECT p.text, username FROM account JOIN " .
+	"(SELECT text, fk_account, `when` FROM comment WHERE fk_product=? ORDER BY `when` LIMIT 5 OFFSET ".$page_num.
+	") AS p ON (account.ID=p.fk_account) ORDER BY p.`when`", [ $_GET["id"] ] );
 $comm = [
 	$comments->fetch(),
 	$comments->fetch(),
@@ -24,6 +24,7 @@ $comm = [
 	$comments->fetch(),
 	$comments->fetch()
 ];
+$lastpage_index = (intval($db("select count(*) from comment where id=?",  [ $_GET["id"] ])->fetch())+4)/5;
 $indexPage = build_default_page("pages/grappa" , [
 	"name"=> $res[0],
 	"desc"=> $res[2],
@@ -37,7 +38,11 @@ $indexPage = build_default_page("pages/grappa" , [
 	"comm4" => $comm[3][0],
 	"user4" => $comm[3][1],
 	"comm5" => $comm[4][0],
-	"user5" => $comm[4][1]
+	"user5" => $comm[4][1],
+	"first_page" => "grappa.php?id=".$_GET["id"]."&page=1",
+	"previous_page" => "grappa.php?id=".$_GET["id"]."&page=".max($page_num-1, 1),
+	"next_page" => "grappa.php?id=".$_GET["id"]."&page=".min($page_num+1, $lastpage_index),
+	"last_page" => "grappa.php?id=".$_GET["id"]."&page=".$lastpage_index
 ]);
 
 
