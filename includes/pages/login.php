394b7166dfcf26f847cf4e784961e16ad65ab1e5
Denis Benato <benato.denis96@gmail.com>
2019-12-16 01:01:18 +0100

    login.php chiama se stessa in post.

--- includes/pages/login.php
+++ includes/pages/login.php
@@ -3,12 +3,13 @@
 define('SITE_ROOT', getcwd());
 include SITE_ROOT."/includes/resources.php";
 
-$username = filter_input(INPUT_POST, "uname");
-$password = hashPassword(filter_input(INPUT_POST, "psw"));
+if (array_key_exists("psw", $_POST)) { // login.php chiama se stessa in POST....
+    $username = filter_input(INPUT_POST, "uname");
+    $password = hashPassword(filter_input(INPUT_POST, "psw"));
 
-// TODO: aggiungere i controlli sul database qui
-if (true) {
-    setUserSession($username, USER_ROLE_USER);
+    if (true) {// TODO: aggiungere i controlli sul database qui
+        setUserSession($username, USER_ROLE_USER);
+    }
 }
 
 $indexPage = build_default_page("pages/login" , []);
