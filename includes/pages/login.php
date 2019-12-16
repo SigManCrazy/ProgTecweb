6b1dcd1ccaef0507410f1cf40d45bf17535f5a61
Denis Benato <benato.denis96@gmail.com>
2019-12-16 01:07:21 +0100

    Login da sistemare..

--- includes/pages/login.php
+++ includes/pages/login.php
@@ -7,7 +7,7 @@ if (array_key_exists("psw", $_POST)) { // login.php chiama se stessa in POST....
     $username = filter_input(INPUT_POST, "uname");
     $password = hashPassword(filter_input(INPUT_POST, "psw"));
 
-    if (true) {// TODO: aggiungere i controlli sul database qui
+    if (checkLogin($username, $password)) {// TODO: aggiungere i controlli sul database qui
         setUserSession($username, USER_ROLE_USER);
     }
 }
--- includes/session.php
+++ includes/session.php
@@ -7,8 +7,8 @@ if (!isUserLogged()) {
     $_SESSION["role"] = null;
 }
 
-define("USER_ROLE_USER", 0);
-define("USER_ROLE_ADMIN", 1);
+define("USER_ROLE_USER", 1);
+define("USER_ROLE_ADMIN", 2);
 
 function isUsernameValid($username) {
     return strlen($username) > 3;
@@ -29,4 +29,17 @@ function setUserSession($username, $role) {
 
 function hashPassword($password) { // Giusto per non salvare le password in chiaro. Se uno dovesse fare le cose fatte bene dovrebbe preoccuparsi di usare un algoritmo non facile di cui fare il reverse in GPU e che impiega un tempo costante.
     return md5("ju84nc73mf3e0".sha1($password));
+}
+
+function checkLogin($username, $password) {
+    $db = new Database();
+	$result = $db("select account.username, account.password, account.type where account.username=? and account.password=?", [
+        $username,
+        $password
+    ]);
+
+    $result = $result->fetch();
+
+    //TODO: non ho PDO. ritornati 0 per nessun utente ed 1 o 2 a seconda del ruolo.
+    return $result[2]; // <= questo ritorna il ruolo, ma lo fa solo in caso di successo.
 }
